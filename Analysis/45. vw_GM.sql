5b85cf5c188266517819752c86705478e63afed5
irfanzim <irfanuzim08@gmail.com>
2026-02-15 07:00:13 +0300

    gross_margin

---
+++ Analysis/45. vw_GM.sql
@@ -0,0 +1,94 @@
+/* The purpose is to join VNSMC and VFC by using keys
+market+fiscal_year */
+
+-- Step 1: Check the data types --
+SELECT 
+    TABLE_NAME, COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME IN ('vw_NSMC','vw_FC')
+    AND COLUMN_NAME IN ('market','fiscal_year')
+    
+-- no issues detected --
+
+-- Step 2: Check for "Hidden" Whitespace or Case insensitivity in market --
+SELECT 
+    'NSMC' AS table_name,
+    COUNT(*) AS non_standard_rows,
+    GROUP_CONCAT(DISTINCT market) AS examples
+FROM vw_NSMC
+WHERE market != UPPER(TRIM(market))
+
+UNION ALL
+
+SELECT 
+    'VFC' AS table_name,
+    COUNT(*) AS non_standard_rows,
+    GROUP_CONCAT(DISTINCT market) AS examples
+FROM vw_fc
+WHERE market != UPPER(TRIM(market))
+
+-- No issues found --
+
+-- Step 3: Check for NULL Join Keys --
+SELECT
+	'NSMC' as table_names,
+    SUM(CASE WHEN market is null THEN 1 else 0 END) as null_market,
+    SUM(CASE WHEN fiscal_year is null THEN 1 else 0 END) as null_fy
+FROM
+	vw_nsmc
+
+UNION ALL
+
+SELECT
+	'VFC' as table_names,
+	SUM(CASE WHEN market is null THEN 1 else 0 END) as null_market,
+    SUM(CASE WHEN fiscal_year is null THEN 1 else 0 END) as null_fy
+FROM
+	vw_fc
+
+-- Null count 0 --
+
+-- Step 4: The Orphan Check (Referential Integrity) --
+
+SELECT DISTINCT
+	f.fiscal_year,
+    f.market
+FROM vw_nsmc f
+LEFT JOIN vw_fc p
+    ON f.market = p.market
+    AND f.fiscal_year=p.fiscal_year
+WHERE p.market IS NULL
+
+-- 0 rows return, referental integrity is intect --
+
+-- Step 5: Row Count Prediction --
+ SELECT 
+    (SELECT COUNT(*) FROM vw_nsmc) AS original_count,
+    (SELECT COUNT(*) 
+     FROM vw_nsmc f
+     INNER JOIN vw_fc p 
+        ON f.market = p.market
+    AND f.fiscal_year=p.fiscal_year) AS joined_count;
+        
+-- row count same --
+
+/* STEP 6: Join both tables. 
+Add freight_cost colcumn. where freight_cost=net_sales*(1-(freight_pct+other_cost_pct))
+Add COGS column, where COGS=manufacturing_cost+(net_sales*(1-(freight_pct+other_cost_pct)))
+Add GM columm, where GM=net_sales-(manufacturing_cost+(net_sales*(1-(freight_pct+other_cost_pct)))) */
+
+Create or Replace View vw_gm as
+
+SELECT
+	f.*,
+    (f.net_sales*(1-(p.freight_pct+p.other_cost_pct))) as freight_cost,
+    (f.manufacturing_cost+(f.net_sales*(1-(p.freight_pct+p.other_cost_pct)))) as cogs,
+    (f.net_sales-(f.manufacturing_cost+(f.net_sales*(1-(p.freight_pct+p.other_cost_pct))))) as gross_margin
+From
+	vw_nsmc f
+INNER JOIN
+	vw_fc p 
+    ON f.market = p.market
+    AND f.fiscal_year=p.fiscal_year
---
+++ Analysis/46. Verify_vw_gm.sql
@@ -0,0 +1,47 @@
+/* The following queries helpe to verify whether joining was successful */
+
+-- Check row counts between vw_nsmc and vw_gm --
+ SELECT       
+	(SELECT count(*) FROM vw_nsmc) as original_count,
+	count(*) as join_count
+FROM 
+	vw_gm
+    
+-- Same count: 1424923 --
+
+-- Check total_sum of net_sales, manufacturing_cost between vw_nsmc and vw_gm --
+SELECT
+	"original" as table_names,
+    sum(net_sales) as total_NS,
+    sum(manufacturing_cost) as total_MC
+FROM
+	vw_nsmc
+
+UNION ALL
+
+SELECT
+	"JOIN" as table_names,
+	sum(net_sales) as total_NS,
+    sum(manufacturing_cost) as total_MC
+FROM
+	vw_gm
+
+-- the value for net_sales is 2875220015.39 in both dataset --
+-- for manufacturing_cost the value is 76306019.29 in both dataset --
+
+-- Count how many rows have NULL gross_margin -- 
+SELECT COUNT(*) AS missing_gm
+FROM vw_gm
+WHERE gross_margin IS NULL;
+
+-- no nulls detected --
+
+-- What are the total_cogs and total_GM--
+SELECT
+	sum(cogs) as total_cogs,
+    sum(gross_margin) as total_gm
+FROM 
+	vw_gm
+    
+-- Total COGS: 2862835899.97 --
+-- Total GM: 12384115.41 --
